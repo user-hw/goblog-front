34073aadaaa12efc733ad98244b28e9cb5b6cc51
HengweiXu <HengweiXu@example.com>
2023-01-13 17:52:46 +0800

    完成页面无限滚动刷新

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -6,15 +6,60 @@ import PersonBox1 from '../Person-box1'
 import PostList from '../Post-list'
 import PostElment from '../PostElement'
 
+import request from 'superagent';
+import debounce from 'lodash.debounce';
+
 
 
 export default class Index extends React.Component {
   constructor(props) {
 		super(props);
 		this.state = {
-			post_list: []
-		}
+      counter : 1,
+      quantity : 5,
+			post_list: [
+      ],
+		};
+
+    window.onscroll = debounce(() => {
+      const {
+        load
+      } = this;
+  
+      if (window.innerHeight + document.documentElement.scrollTop >= document.documentElement.offsetHeight) {
+        this.load();
+      }
+    }, 100);
 	}
+
+  componentDidMount(){
+    this.load()
+  }
+
+  load(){
+    console.log("调用了load")
+    let a =this.state.post_list
+    for (let index = 0; index < 5; index++) {
+      a.push({post:1})
+    }
+    console.log(a)
+
+    this.setState({
+      post_list: a,
+    })
+    // console.log("this.state.post_list ==== ",this.state)
+    
+    // const postElement = document.createElement('PostElment');
+    // console.log(postElement)
+    // title.innerText='Hello React (method 1)';
+    // title.className='main';
+    // document.getElementById('Index').appendChild(postElement);
+    // ReactDOM.render(postElement, document.getElementById('Index'));
+  }
+  
+
+
+ 
   render(){
   return (
     <div>
@@ -23,6 +68,14 @@ export default class Index extends React.Component {
       </div>
       <br />
       <div id='indexPageList' className='IndexPageList'>
+
+      {/* <React.Fragment>
+              <p>{this.state.post_list}</p>
+          </React.Fragment> */}
+      {this.state.post_list.map((item, index) => {
+						return (
+                <PostElment />
+						)})}
 
       </div>
         
--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -2,19 +2,57 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Hello from '../Hello';
 
-export default class Test extends React.Component{
-  handleGetDom = () => {
-    let title = document.querySelector('#title');
-    ReactDOM.findDOMNode(title).style.background = 'green'
+// export default class Test extends React.Component{
+//   handleGetDom = () => {
+//     let title = document.querySelector('#title');
+//     ReactDOM.findDOMNode(title).style.background = 'green'
+//   }
+
+//   render() {
+//       return (
+//           <>
+//               <h1 id="title">测试节点</h1>
+//               <button onClick={this.handleGetDom}>点击操作Dom</button>
+//               <Hello test={"nihao"}/>
+//           </>
+//       )
+//   }
+// }
+
+
+export default class Test extends React.Component {
+  constructor(props) {
+      super(props);
+      this.state = {
+          testArr: ["a",'b','b']
+      };
   }
 
-  render() {
+  componentDidMount(){
+    let a=this.state.testArr
+    for (let index = 0; index < 5; index++) {
+      a = [...a,'a']
+    // this.state.testArr = ['c','d']
+    console.log('------------')
+    }  
+
+    this.setState({
+      testArr:a
+    })
+    setTimeout(() => {
+      console.log(this.state.testArr);
+    }, 1000); 
+    console.log(this.state.testArr)
+  }
+  render(){
+    // let {elem} =this.state.testArr
       return (
-          <>
-              <h1 id="title">测试节点</h1>
-              <button onClick={this.handleGetDom}>点击操作Dom</button>
-              <Hello test={"nihao"}/>
-          </>
+        <div>
+        {/* <h1>{elem}</h1> */}
+          <React.Fragment>
+              <p>{this.state.testArr}</p>
+          </React.Fragment>
+          </div>
       )
   }
 }
