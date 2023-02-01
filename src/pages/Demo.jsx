ee430672872d4afc56477d7b5c5de1da57c17a10
xu hengwei <1761173100@qq.com>
2023-02-01 11:26:23 +0800

    更新管理员界面

--- src/pages/Demo.jsx
+++ src/pages/Demo.jsx
@@ -1,19 +1,98 @@
-import React from "react";
-import { Layout } from '@feb-team/legao-react';
-import '@feb-team/legao-react/dist/styles/css/legao.all.css';
-import HeaderAdmin from "../Header-admin";
+import {
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+  UploadOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+} from '@ant-design/icons';
+import { Layout, Menu, theme } from 'antd';
+import React, { useState } from 'react';
+
+import HeaderAdmin from '../Header-admin';
 
 
-export default function Demo (){
-    const { Header, Content, Footer, Sider } = Layout
-    return (
-      <Layout >
-        <Header><HeaderAdmin/></Header>
-        <Layout hasSider>
-          <Sider>Sider</Sider>
-          <Content>Content</Content>
-        </Layout>
-        <Footer>Footer</Footer>
-      </Layout>
-    )
+
+
+const { Header, Sider, Content } = Layout;
+const App = () => {
+
+  const changePage=(value)=>{
+    console.log("修改页面value===",value)
   }
+
+  const [collapsed, setCollapsed] = useState(false);
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
+  return (
+    <div>
+    <Layout>
+      <HeaderAdmin/>
+    </Layout>
+
+    <Layout style={{'margin':' 80px auto 0 auto'}}>  
+      <Sider trigger={null} collapsible collapsed={collapsed} breakpoint="md"
+        onBreakpoint={(broken) => {
+          console.log(broken);
+          setCollapsed(broken)
+        }}
+        >
+        <div className="logo" />
+        <Menu
+          onClick={changePage}
+          theme="dark"
+          mode="inline"
+          defaultSelectedKeys={['1']}
+          items={[
+            {
+              key: '1',
+              icon: <UserOutlined />,
+              label: 'nav 1',
+            },
+            {
+              key: '2',
+              icon: <VideoCameraOutlined />,
+              label: 'nav 2',
+            },
+            {
+              key: '3',
+              icon: <UploadOutlined />,
+              label: 'nav 3',
+            },
+            {
+              key: '4',
+              icon: <UploadOutlined />,
+              label: 'nav 4',
+            },
+          ]}
+        />
+      </Sider>
+      <Layout className="site-layout">
+        <Header
+          style={{
+            padding: 0,
+            background: colorBgContainer,
+          }}
+        >
+          {React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
+            className: 'trigger',
+            onClick: () => setCollapsed(!collapsed),
+          })}
+        </Header>
+        <Content
+          style={{
+            margin: '24px 16px',
+            padding: 24,
+            minHeight: 280,
+            background: colorBgContainer,
+          }}
+        >
+          Content1
+          
+        </Content>
+      </Layout>
+    </Layout>
+    </div>
+  );
+};
+export default App;
