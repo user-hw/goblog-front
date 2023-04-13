f2702fb445412019ea8330a9df26473b067b427a
xu hengwei <1761173100@qq.com>
2023-04-14 00:48:27 +0800

    更新主页界面，改好看了点

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -5,6 +5,7 @@ import Hello from '../Hello'
 import PersonBox1 from '../Person-box1'
 import PostList from '../Post-list'
 import PostElment from '../PostElement'
+import MasonryImageList from '../MasonryImageList'
 
 import request from 'superagent';
 import debounce from 'lodash.debounce';
@@ -13,6 +14,14 @@ import { useHistory,useLocation } from 'react-router-dom';
 
 import axios from 'axios'
 
+// import * as React from 'react';
+import Box from '@mui/material/Box';
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
+import ImageListItemBar from '@mui/material/ImageListItemBar';
+import ListSubheader from '@mui/material/ListSubheader';
+import IconButton from '@mui/material/IconButton';
+import InfoIcon from '@mui/icons-material/Info';
 /*遗留问题
 滑动到底后显示已经到底的标识
 
@@ -26,7 +35,7 @@ export default class Index extends React.Component {
 			post_list: [
       ],
       pageNum:1,
-      postNum:5,
+      postNum:8,
 		};
 
 
@@ -40,7 +49,7 @@ export default class Index extends React.Component {
       console.log(document.documentElement.scrollTop)
       console.log(document.documentElement.offsetHeight)
   
-      if (window.innerHeight + document.documentElement.scrollTop >= (document.documentElement.offsetHeight-5) && this.isBottom()===false ) {
+      if (window.innerHeight + document.documentElement.scrollTop >= (document.documentElement.offsetHeight-5) ) {
         this.state.pageNum +=1
         this.load();
         
@@ -48,8 +57,8 @@ export default class Index extends React.Component {
       }else if (this.isBottom()===true ){
         console.log("已经到底了")
   
-        let loginEnd = document.querySelector('.isBottom');
-        loginEnd.style.display = 'block';	// 显示选择的元素
+        // let loginEnd = document.querySelector('.isBottom');
+        // loginEnd.style.display = 'block';	// 显示选择的元素
       }
     }, 100);
 	}
@@ -59,7 +68,7 @@ export default class Index extends React.Component {
   }
 
   isBottom(){
-    if (this.state.pageNum<=4){
+    if (this.state.pageNum<=40){
       return false
     }else{
       return true
@@ -101,15 +110,53 @@ export default class Index extends React.Component {
       </div>
       <br />
 
-      <div id='MainPage' className='MainPage'>
+      <Box sx={{ width: "100%", height: '100%'}}>
+        
+      <ImageList variant="masonry" cols={3} gap={8}>
+        {this.state.post_list.map((item) => (
+          <ImageListItem key={item.blogPost.titleImg} href={"www.baidu.com"}>
+            <a href={"/post/" +item.blogPost.ID}class="post-headline">
+            <img
+              src={`${item.blogPost.titleImg}`}
+              srcSet={`${item.blogPost.titleImg}&dpr=2 2x`}
+            //   src={`${item.img}?w=248&fit=crop&auto=format`}
+            //   srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
+              alt={item.blogPost.title}
+              
+              
+              loading="lazy"
+            />
 
-      {this.state.post_list.map((item, index) => {
+          <ImageListItemBar
+            title={item.blogPost.title}
+            subtitle={'@'+item.NickName}
+            actionIcon={
+              <IconButton
+                sx={{ color: 'rgba(255, 255, 255, 0.54)' }}
+                aria-label={`info about ${item.title}`}
+              >
+                <InfoIcon />
+              </IconButton>
+            }
+          />
+            </a>
+          </ImageListItem>
+        ))}
+      </ImageList>
+    </Box>
+
+      {/* <MasonryImageList items={this.state.post_list}/> */}
+
+      <div id='MainPage' className='MainPage'>
+      
+
+      {/* {this.state.post_list.map((item, index) => {
 						return (
                 <PostElment data={item}/>
-						)})}
+						)})} */}
       
       </div>
-      <p id='isBottom' style={{"display":"none"}}>啊哦，已经到底了</p>
+      {/* <p id='isBottom' style={{"display":"none"}}>啊哦，已经到底了</p> */}
     </div>
   )
   }
